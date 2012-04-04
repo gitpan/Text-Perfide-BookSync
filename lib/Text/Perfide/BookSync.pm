25e854ed8d80aa4aa720dde38107158ca8b8f65f
André Fernandes dos Santos <andrefs@cpan.org>
2012-04-04 16:16:22 +0000

    Import of ANDREFS/Text-Perfide-BookSync-0.01_02 from CPAN.
    gitpan-cpan-distribution: Text-Perfide-BookSync
    gitpan-cpan-version:      0.01_02
    gitpan-cpan-path:         ANDREFS/Text-Perfide-BookSync-0.01_02.tar.gz
    gitpan-cpan-author:       ANDREFS
    gitpan-cpan-maturity:     developer

--- lib/Text/Perfide/BookSync.pm
+++ lib/Text/Perfide/BookSync.pm
@@ -4,9 +4,13 @@ use 5.006;
 use strict;
 use warnings;
 use Data::Dumper;
+use Data::Dump qw/dump/;
 use List::Util qw/min/;
 use HTML::Auto qw/matrix h v/;
 use File::Basename;
+use utf8::all;
+use feature qw/say/;
+
 
 =head1 NAME
 
@@ -14,14 +18,21 @@ Text::Perfide::BookSync - The great new Text::Perfide::BookSync!
 
 =head1 VERSION
 
-Version 0.01_01
+Version 0.01_02
 
 =cut
 
 use base 'Exporter';
-our @EXPORT = (qw/htmlmatrix marksync splitchunks calchunks populate moreinfosecs moreinfochunks/);
+our @EXPORT = (qw/	htmlmatrix
+					marksync
+					splitchunks
+					calchunks
+					populate
+					moreinfosecs
+					moreinfochunks
+				/);
 
-our $VERSION = '0.01_01';
+our $VERSION = '0.01_02';
 
 =head1 SYNOPSIS
 
@@ -112,9 +123,9 @@ sub htmlmatrix{
 	if(defined($dir))	{ $dir.= "/"; }
 	else				{ $dir = "";  }
 
-	open HTML,'>',"$dir$fileL"."_$fileR.html" or die "Could not open file '$dir${fileL}_$fileR.html' for writing!";
+	open my $html,'>',"$dir$fileL"."_$fileR.html" or die "Could not open file '$dir${fileL}_$fileR.html' for writing!";
 	my $m = matrix(\@cols,\@lines,$data);
-	print HTML v(h($m));
+	print $html v(h($m));
 }
 
 =head2 marksync
@@ -137,30 +148,41 @@ sub marksync{
 		$fR = $fileR;
 	}
 
-	open(SYNCL,'>',"$fL.sync") or die "Could not open file '$fileL.sync'";
-	open(SYNCR,'>',"$fR.sync") or die "Could not open file '$fileR.sync'";
+	open my $syncl,'>',"$fL.sync" or die "Could not open file '$fileL.sync'";
+	open my $syncr,'>',"$fR.sync" or die "Could not open file '$fileR.sync'";
 
 	push @{$options->{outlist}},"$fL.sync\t$fR.sync" if defined($options->{outfile});
 
 	# Print first section mark
 	my $rm = $options->{rm} // 0;
-	print SYNCL qq/<sync id="0">\n/ unless $rm>0;
-	print SYNCR qq/<sync id="0">\n/ unless $rm>0;
+	print $syncl qq/<sync id="0">\n/ unless $rm>0;
+	print $syncr qq/<sync id="0">\n/ unless $rm>0;
+
+
+	open my $fhL, '<', $fL or die;
+	open my $fhR, '<', $fR or die;
+	my $t;
 
 	# For each chunk 
 	for(my $i=$rm; $i < @$chunks; $i++){
 
+		my ($ltext,$rtext);
+
 		# Get the chunk's start offset and end offset (LEFT)
-		my $lstart = $chunks->[$i]{left}{start};
-		my $lend   = $chunks->[$i]{left}{end};
+		my $l_start  = $chunks->[$i]{left}{start};
+		my $l_length = $chunks->[$i]{left}{end} - $chunks->[$i]{left}{start};
 		# Get the chunk's text (LEFT)
-		my $ltext  = qx{head -c $lend '$fileL' | tail -c +$lstart};
+		seek($fhL,0,0);
+		read($fhL,$ltext,$l_start);
+		read($fhL,$ltext,$l_length);
 
 		# Get the chunk's start offset and end offset (RIGHT)
-		my $rstart = $chunks->[$i]{right}{start};
-		my $rend   = $chunks->[$i]{right}{end};
-		# Get the chunk's text (RIGHT)
-		my $rtext  = qx{head -c $rend '$fileR' | tail -c +$rstart};
+		my $r_start  = $chunks->[$i]{right}{start};
+		my $r_length = $chunks->[$i]{right}{end} - $chunks->[$i]{right}{start};
+		# Get the chunk's text (REFT)
+		seek($fhR,0,0);
+		read($fhR,$rtext,$r_start);
+		read($fhR,$rtext,$r_length);
 
 		# if both sides start with section mark, put sync mark
 		if ($ltext =~ /^\s*_sec[^:]*:([^_]+)_/ and $rtext =~ /^\s*_sec[^:]*:([^_]+)_/){
@@ -173,15 +195,15 @@ sub marksync{
 		$rtext =~ s/_sec[^:]*:([^_]+)_//g unless $options->{noclean};
 
 		# Print to .sync file
-		print SYNCL $ltext;
-		print SYNCR $rtext;
+		print $syncl $ltext;
+		print $syncr $rtext;
 	}
 
-	print SYNCL qq{</sync>\n};
-	print SYNCR qq{</sync>\n};
+	print $syncl qq{</sync>\n};
+	print $syncr qq{</sync>\n};
 
-	close SYNCL;
-	close SYNCR;
+	close $syncl;
+	close $syncr;
 }
 
 =head2 splitchunks
@@ -193,14 +215,30 @@ Given two files FILEL and FILER, splits them by their synchronization points, st
 sub splitchunks{
 	my ($chunks,$fileL,$fileR) = @_;
 	my $ch=1;
-	for(@$chunks){
+	open my $fhL, '<', $fileL or die;
+	open my $fhR, '<', $fileR or die;
+	for my $c (@$chunks){
 		my $id = sprintf("%.3d",int($ch));
-		my $lstart = $_->{left}{start};
-		my $rstart = $_->{right}{start};
-		my $lend = $_->{left}{end};
-		my $rend= $_->{right}{end};
-		qx{head -c $lend '$fileL' | tail -c +$lstart > $fileL.c$id};
-		qx{head -c $rend '$fileR' | tail -c +$rstart > $fileR.c$id};
+		my ($t,$fout);
+
+		my $l_start  = $c->{left}{start};
+		my $l_length = $c->{left}{end} - $c->{left}{start};
+		seek($fhL,0,0);
+		read($fhL,$t,$l_start); # because seek only works in bytes
+		read($fhL,$t,$l_length);
+		open $fout, '>', "$fileL.c$id" or die;
+		print $fout $t;
+		close $fout;
+
+		my $r_start  = $c->{right}{start};
+		my $r_length = $c->{right}{end} - $c->{right}{start};
+		seek($fhR,0,0);
+		read($fhR,$t,$r_start); # because seek only works in bytes
+		read($fhR,$t,$r_length);
+		open $fout, '>', "$fileR.c$id" or die;
+		print $fout $t;
+		close $fout;
+
 		$ch++;	
 	}
 }
@@ -220,24 +258,24 @@ sub calchunks{
 	if(defined($dir))	{ $dir.= "/"; }
 	else				{ $dir = "";  }
 
-	open SECSL,'>',"$dir$fL.secs" or die "Can't open file '$dir$fL.secs' for writing!";
-	open SECSR,'>',"$dir$fR.secs" or die "Can't open file '$dir$fR.secs' for writing!";
+	open my $secsL,'>',"$dir$fL.secs" or die "Can't open file '$dir$fL.secs' for writing!";
+	open my $secsR,'>',"$dir$fR.secs" or die "Can't open file '$dir$fR.secs' for writing!";
 	if ($options->{num}){ ## Compare only section numbers
-		map {my $x = $_->{id}; $x =~ s/.*=//; print SECSL $x,"\n"} @{$tabsec->{left}{secs}};
-		map {my $x = $_->{id}; $x =~ s/.*=//; print SECSR $x,"\n"} @{$tabsec->{right}{secs}};
+		map {my $x = $_->{id}; $x =~ s/.*=//; print $secsL $x,"\n"} @{$tabsec->{left}{secs}};
+		map {my $x = $_->{id}; $x =~ s/.*=//; print $secsR $x,"\n"} @{$tabsec->{right}{secs}};
 	}
 	else{
-		map {print SECSL $_->{id},"\n"} @{$tabsec->{left}{secs}};
-		map {print SECSR $_->{id},"\n"} @{$tabsec->{right}{secs}};
+		map {print $secsL $_->{id},"\n"} @{$tabsec->{left}{secs}};
+		map {print $secsR $_->{id},"\n"} @{$tabsec->{right}{secs}};
 	}
 	
 	my ($l,$r) = (-1,-1);
-	my $diff = "$dir${fL}_$fR.diff";
-	qx{diff -y "$dir$fL.secs" "$dir$fR.secs" > '$diff'};
-	open DIFF,"<", "$diff";
+	my $diff_file = "$dir${fL}_$fR.diff";
+	qx{diff -y "$dir$fL.secs" "$dir$fR.secs" > '$diff_file'};
+	open my $diff,"<", "$diff_file";
 	my $chunks = [];
 
-	while(<DIFF>){
+	while(<$diff>){
 		chomp;
 		my @a = split /\t+/;	
 		if    ($a[1] =~ /^\s*<$/)   {
@@ -273,7 +311,7 @@ sub calchunks{
 		}
 	}
 
-	unlink("$fL.secs","$fR.secs",$diff) unless defined($options->{dump});
+	unlink("$fL.secs","$fR.secs",$diff_file) unless defined($options->{dump});
 		
 	$chunks->[-1]{left}{end} = $tabsec->{left}{secs}[-1]{end};
 	$chunks->[-1]{right}{end} = $tabsec->{right}{secs}[-1]{end};
@@ -294,8 +332,8 @@ From a given file in which sections have been delimited with Text::Perfide::Book
 sub populate{
 	my ($file) = shift;
 	my (@idlist,$text);
-	open(FILE,$file) or die "Could not open file '$file'";
-	$text = join '',<FILE>;
+	open my $fh, '<', $file or die "Could not open file '$file'";
+	$text = join '',<$fh>;
 	
 	push @idlist, { 'id' => 'begin', 'start' => 0, title => 'begin' };
 	while($text =~ /(_sec.*:)(.*?_)/g){
@@ -307,7 +345,8 @@ sub populate{
 
 		# Get title
 		my $subs = substr($text,$-[0],200);
-		if ($subs =~ /.*\n*(.|\n){5,100}?(?=\n)/pg) 	{ $idlist[-1]->{title} = ${^MATCH}; }
+		# if ($subs =~ /.*\n*(.|\n){5,100}?(?=\n)/pg) 	{ $idlist[-1]->{title} = ${^MATCH}; }
+		if ($subs =~ /.*\s+([^\n]{1,100})/g) 				{ $idlist[-1]->{title} = ${^MATCH}; }
 		else											{ $idlist[-1]->{title} = '' 		}
 	}
 	$idlist[-1]{end} = length $text;
@@ -321,15 +360,36 @@ Calculates metrics on each pair of sections (length in words, ...)
 =cut
 
 sub moreinfosecs{
-	my $tabsec = shift;
-	for(@{$tabsec->{left}{secs}}){
-		$_->{wc} = qx!head -c $_->{end} '$tabsec->{left}{file}' | tail -c +$_->{start} | wc -w!;
-		chomp $_->{wc};
+	my ($tabsec,$options) = @_;
+	my ($fileL,$fileR) = ($tabsec->{left}{file},$tabsec->{right}{file});
+	open my $fhL, '<', $fileL;
+	open my $fhR, '<', $fileR;
+	my $t;
+
+	for my $sec (@{$tabsec->{left}{secs}}){
+		my $start  = $sec->{start};
+		my $length = $sec->{end} - $sec->{start};
+		seek($fhL,0,0);
+		read($fhL,$t,$start); # because seek only works in bytes
+		read($fhL,$t,$length);
+		$sec->{wc} = split /\s+/,$t;
 	}
-	for(@{$tabsec->{right}{secs}}){
-		$_->{wc} = qx!head -c $_->{end} '$tabsec->{right}{file}' | tail -c +$_->{start} | wc -w!;
-		chomp $_->{wc};
+
+	for my $sec (@{$tabsec->{right}{secs}}){
+		my $start  = $sec->{start};
+		my $length = $sec->{end} - $sec->{start};
+		seek($fhL,0,0);
+		read($fhL,$t,$start); # because seek only works in bytes
+		read($fhL,$t,$length);
+		$sec->{wc} = split /\s+/,$t;
 	}
+
+	my $dir = $options->{dir};
+	if(defined($dir))   { $dir.= "/"; }
+	else                { $dir = "";  }
+	my ($fL,$fR) = (basename($tabsec->{left}{file}), basename($tabsec->{right}{file}));
+
+	_dump2file($tabsec,"$dir${fL}_$fR.tabsec") if ($options->{dump});
 }
 
 =head2 moreinfochunks
